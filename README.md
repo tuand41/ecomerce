97ede39472e0987a5c88b60de5e50916f9d63340
tuand41 <tuan.nguyen4@asiantech.vn>
2021-05-02 14:14:37 +0700

    auth-scaffolding-bootstrap

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 php-sniffer
     ./vendor/bin/phpcs -n --standard=phpcs.xml
     ./vendor/bin/phpcbf -n --standard=phpcs.xml
-
-test
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('12345678'),
+        ]);
+    }
+}
