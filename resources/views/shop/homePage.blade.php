1137c2cdb196f59ab67c99fc9e573b022cda99b5
tuan <tuan@example.com>
2022-06-07 22:06:56 +0700

    "load products"

--- resources/views/shop/homePage.blade.php
+++ resources/views/shop/homePage.blade.php
@@ -7,12 +7,13 @@
             <li class="list-group-item list-group-item-info text-center">Sản phẩm mới</li>
         </h3>
         <div class="row">
+            @foreach ($products as $product)
             <div class="col-sm-6 col-md-3">
                 <div class="thumbnail">
-                    <img src="images/sp1.jpg" alt="..." width="100%">
+                    <img src="{{ $product->image }}" alt="..." width="100%">
                     <div class="caption ">
-                        <h3>Đàn Guitar Acoustic Mantic AG 370</h3>
-                        <p>Giá: 880.000 VNĐ</p>
+                        <h3> {{ $product->name }} </h3>
+                        <p>Giá: {{ $product->price }} VNĐ</p>
                         <p>
                             <a href="#" class="btn btn-default" role="button">Chi Tiết</a>
                             <a href="#" class="btn btn-primary" role="button">Mua Ngay</a>
@@ -20,105 +21,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-sm-6 col-md-3">
-                <div class="thumbnail">
-                    <img src="images/sp1.jpg" alt="..." width="100%">
-                    <div class="caption ">
-                        <h3>Đàn Guitar Acoustic Mantic AG 370</h3>
-                        <p>Giá: 880.000 VNĐ</p>
-                        <p>
-                            <a href="#" class="btn btn-default" role="button">Chi Tiết</a>
-                            <a href="#" class="btn btn-primary" role="button">Mua Ngay</a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-md-3">
-                <div class="thumbnail">
-                    <img src="images/sp1.jpg" alt="..." width="100%">
-                    <div class="caption ">
-                        <h3>Đàn Guitar Acoustic Mantic AG 370</h3>
-                        <p>Giá: 880.000 VNĐ</p>
-                        <p>
-                            <a href="#" class="btn btn-default" role="button">Chi Tiết</a>
-                            <a href="#" class="btn btn-primary" role="button">Mua Ngay</a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-md-3">
-                <div class="thumbnail">
-                    <img src="images/sp1.jpg" alt="..." width="100%">
-                    <div class="caption ">
-                        <h3>Đàn Guitar Acoustic Mantic AG 370</h3>
-                        <p>Giá: 880.000 VNĐ</p>
-                        <p>
-                            <a href="#" class="btn btn-default" role="button">Chi Tiết</a>
-                            <a href="#" class="btn btn-primary" role="button">Mua Ngay</a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <hr>
-    <div class="spbanchay">
-        <h3>
-            <li class="list-group-item list-group-item-info text-center">Sản phẩm nổi bật</li>
-        </h3>
-        <div class="row">
-            <div class="col-sm-6 col-md-3">
-                <div class="thumbnail">
-                    <img src="images/sp1.jpg" alt="..." width="100%">
-                    <div class="caption ">
-                        <h3>Đàn Guitar Acoustic Mantic AG 370</h3>
-                        <p>Giá: 880.000 VNĐ</p>
-                        <p>
-                            <a href="#" class="btn btn-default" role="button">Chi Tiết</a>
-                            <a href="#" class="btn btn-primary" role="button">Mua Ngay</a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-md-3">
-                <div class="thumbnail">
-                    <img src="images/sp1.jpg" alt="..." width="100%">
-                    <div class="caption ">
-                        <h3>Đàn Guitar Acoustic Mantic AG 370</h3>
-                        <p>Giá: 880.000 VNĐ</p>
-                        <p>
-                            <a href="#" class="btn btn-default" role="button">Chi Tiết</a>
-                            <a href="#" class="btn btn-primary" role="button">Mua Ngay</a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-md-3">
-                <div class="thumbnail">
-                    <img src="images/sp1.jpg" alt="..." width="100%">
-                    <div class="caption ">
-                        <h3>Đàn Guitar Acoustic Mantic AG 370</h3>
-                        <p>Giá: 880.000 VNĐ</p>
-                        <p>
-                            <a href="#" class="btn btn-default" role="button">Chi Tiết</a>
-                            <a href="#" class="btn btn-primary" role="button">Mua Ngay</a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-md-3">
-                <div class="thumbnail">
-                    <img src="images/sp1.jpg" alt="..." width="100%">
-                    <div class="caption ">
-                        <h3>Đàn Guitar Acoustic Mantic AG 370</h3>
-                        <p>Giá: 880.000 VNĐ</p>
-                        <p>
-                            <a href="#" class="btn btn-default" role="button">Chi Tiết</a>
-                            <a href="#" class="btn btn-primary" role="button">Mua Ngay</a>
-                        </p>
-                    </div>
-                </div>
-            </div>
+            @endforeach            
         </div>
     </div>
 </div> <!-- /.col-xs-12.col-md-3.center -->
